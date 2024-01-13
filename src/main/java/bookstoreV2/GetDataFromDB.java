d9de4a11183726fc62037c77a274a51540ff007c
ndimovt <nikolaitodorov119@gmail.com>
2024-01-13 12:22:32 +0200

    -changed try with try-with-resources -removed close() method

--- src/main/java/bookstoreV2/GetDataFromDB.java
+++ src/main/java/bookstoreV2/GetDataFromDB.java
@@ -29,11 +29,9 @@ public class GetDataFromDB {
         return con;
     }
     protected void addBookToDB(String bName, String aName, int year, double bookPrice){
-        Connection DBconnection = null;
-        PreparedStatement ps = null;
-        try {
-            DBconnection = this.getConnection();
-            ps = DBconnection.prepareStatement("call bookstore.add_book(?,?,?,?)");
+        try (Connection DBconnection = this.getConnection();
+             PreparedStatement ps = DBconnection.prepareStatement("call bookstore.add_book(?,?,?,?)"))
+        {
             ps.setString(1, aName);
             ps.setString(2, bName);
             ps.setInt(3, year);
@@ -45,27 +43,20 @@ public class GetDataFromDB {
         }catch (InputMismatchException imme){
             System.out.println("Wrong value entered");
             imme.printStackTrace();
-        }finally {
-            closeConnection(DBconnection,ps,null);
         }
     }
     protected ArrayList<Book> bookInfo(){
         ArrayList<Book> bookData = new ArrayList<>();
-        Connection c = null;
-        PreparedStatement pst = null;
-        ResultSet rs = null;
-        try {
-            c = this.getConnection();
-            pst = c.prepareStatement(" call bookstore.get_info()");
-            rs = pst.executeQuery();
+        try (Connection c = this.getConnection();
+             PreparedStatement pst = c.prepareStatement(" call bookstore.get_info()");
+             ResultSet rs = pst.executeQuery())
+        {
             while (rs.next()) {
                 Book book = new Book(rs.getString("author_name"), rs.getString("book_title"), rs.getInt("release_year"), rs.getDouble("price"));
                 bookData.add(book);
             }
         } catch (SQLException SE){
             SE.printStackTrace();
-        } finally{
-            closeConnection(c,pst, rs);
         }
         return bookData;
     }
@@ -79,37 +70,18 @@ public class GetDataFromDB {
         ArrayList<Book> bookData = bookInfo();
         for (Book changedBook : bookData) {
             if (bookTitle.equals(changedBook.getBookName())) {
-                Connection DBCon = null;
-                PreparedStatement pst = null;
-                try {
-                    DBCon = this.getConnection();
-                    pst = DBCon.prepareStatement("call bookstore.change_price(?,?)");
+                try (Connection DBCon =  this.getConnection();
+                     PreparedStatement pst = DBCon.prepareStatement("call bookstore.change_price(?,?)"))
+                {
                     pst.setString(1, bookTitle);
                     pst.setDouble(2, changedBook.setPrice(updatedPrice));
                     pst.executeUpdate();
                 }catch (SQLException sqe){
                     System.out.println("Connection error!");
                     sqe.printStackTrace();
-                }finally {
-                    closeConnection(DBCon,pst,null);
                 }
             }
         }
         return bookData;
-    }
-    private void closeConnection(Connection con, PreparedStatement pst, ResultSet re) {
-        try {
-            if (con != null) {
-                con.close();
-            }
-            if (pst != null) {
-                pst.close();
-            }
-            if (re != null) {
-                re.close();
-            }
-        } catch (SQLException E) {
-            System.out.println("No connection");
-        }
     }
 }
